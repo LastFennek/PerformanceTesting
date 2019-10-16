386619bca043a04be1fcdff319f1076bc0a2e856
[LastFennek] <[scherzma@gmail.com]>
2019-10-16 12:56:43 +0200

    TreeMap Read Test Added

--- src/com/Controll/Main.java
+++ src/com/Controll/Main.java
@@ -9,24 +9,33 @@ public class Main {
     public static void main(String[] args) {
         Timer timer = new BasicTimer();
 
-        Test testGraph = new GraphCreatingTest(10_000,100,250);
+        //Test testGraph = new GraphCreatingTest(10_000,100,250);
 
-        Test testArrayWrite = new ArrayWriteSpeedTest(50_000_000);
+        //Test testArrayWrite = new ArrayWriteSpeedTest(50_000_000);
 
-        Test arrTest = new BasicArray(true, 50_000_000);
+        //Test arrTest = new BasicArray(true, 50_000_000);
 
-        BasicArray arr = new BasicArray(true,50_000_000);
-        Test testArrayRead = new ArrayReadSpeedTest(arr.getArray());
+        //BasicArray arr = new BasicArray(true,50_000_000);
+        //Test testArrayRead = new ArrayReadSpeedTest(arr.getArray());
 
-        Test BasicTreeMap = new BasicTreeMap(100_000);
+        Test BasicTreeMap = new BasicTreeMap(10_000_000);
+
+        Test bitSetTest = new BasicBitSet(1000);
+
+        System.out.println("Everything Created!");
+        timer.startTimer();
+        bitSetTest.startTest(2);
+        timer.endTimer(2);
+        System.out.println("Read BitSet\n");
+
 
 
         timer.startTimer();
-        BasicTreeMap.startTest(200);
-        timer.endTimer(200);
+        BasicTreeMap.startTest(2);
+        timer.endTimer(2);
         System.out.println("Read TreeMap\n");
 
-/*
+        /*
         timer.startTimer();
         testGraph.startTest(20);
         timer.endTimer(20);
--- src/com/Tests/BasicTreeMap.java
+++ src/com/Tests/BasicTreeMap.java
@@ -2,7 +2,6 @@ package com.Tests;
 
 import java.util.Hashtable;
 import java.util.TreeMap;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class BasicTreeMap implements Test{
@@ -23,33 +22,20 @@ public class BasicTreeMap implements Test{
 
 
     private void fillHashMap(){
-        int x = 0;
         for (int i = 0; i < size; i++){
-            x = Math.abs(rand.nextInt());
-            while(tm.containsKey(x)){
-                x = Math.abs(rand.nextInt());
-            }
-            tm.put(x,Math.abs(rand.nextInt()));
+            tm.put(Math.abs(rand.nextInt()),Math.abs(rand.nextInt()));
         }
     }
 
     private void readHashMap(){
-        int v = 0;
-        try{
-            int x = 0;
-            TreeMap<Integer,Integer> tmCopy = new TreeMap<>(){{putAll(tm);}};
-            for (int i = 0; i < size; i++){
-                x += tmCopy.get(tmCopy.firstKey());
-                tmCopy.remove(tmCopy.firstKey());
-                v = i;
-                //tm.put(tm.firstKey(),tm.get(tm.firstKey())+1); Test ob das Wirklich stuff macht.
-                //System.out.println(x);
-            }
-        }catch (Exception e){
-            System.out.println(v + "  "+e);
+        int x = 0;
+        TreeMap<Integer,Integer> tmCopy = new TreeMap<>(){{putAll(tm);}};
+        for (int i = 0; i < size; i++){
+            x += tmCopy.get(tmCopy.firstKey());
+            tmCopy.remove(tmCopy.firstKey());
         }
-
-
+        tm.put(tm.firstKey(),tm.get(tm.firstKey())+1);
+        //System.out.println(x);
     }
 
 
