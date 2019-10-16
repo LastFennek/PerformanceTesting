daff25209961e304be784034b40b0f2d4396f188
[LastFennek] <[scherzma@gmail.com]>
2019-10-16 13:01:52 +0200

    TreeMap Read Test Added

--- src/com/Graph/Graph.java
+++
@@ -1,7 +0,0 @@
-package com.Graph;
-
-public interface Graph {
-    void printArray();
-    void printArray(int[][] arr);
-    int[][] getArray();
-}
--- src/com/Timer/Timer.java
+++
@@ -1,7 +0,0 @@
-package com.Timer;
-
-public interface Timer {
-    void startTimer();
-    void endTimer();
-    void endTimer(int times);
-}
